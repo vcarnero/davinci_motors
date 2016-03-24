6d4722197cab408928800079318d4eb87b01d76b
Veronica Carnero <vcarnero@gmail.com>
2016-03-24 10:40:04 -0600

    some things I missed and fixed after watching video.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,19 +8,18 @@ class CarsController < ApplicationController
   end
 
   def create
-
     @car = Car.new(car_params)
     creation_message = "#{@car.year} #{@car.make} #{@car.model} created"
     if @car.save
-    redirect_to cars_path, notice: creation_message
+      redirect_to cars_path, notice: creation_message
     else
       render :new
+    end
   end
-end
+
   private
 
   def car_params
     params.require(:car).permit([:year, :make, :model, :price])
   end
 end
-
